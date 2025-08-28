d057c530926e9d894797bb357d66e42468719371
Vikram Kumar Matte <mattevikramkumar@gmail.com>
2025-08-27 22:30:01 -0500

    Update streamlit_app.py
    Updated the display note from smoothie info to breakfast menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,9 +5,11 @@ from snowflake.snowpark.functions import col
 
 
 # Write directly to the app
-st.title(f":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
+st.title(f"My Parents New Healthy Diner")
 st.write(
-  """Choose the fruits you want in your custom Smoothie!
+  """Omega 3 & Blueberry Oatmeal
+  Kale, Spinach & Rocket Smoothie
+  Hard-Boiled Free-Range Egg
   """)
 
 
